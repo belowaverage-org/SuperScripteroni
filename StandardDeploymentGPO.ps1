bcd968f4510b85cc702f03dc4a1c744aec8ee9bf
Dylan Bickerstaff <dylan@belowaverage.org>
2019-06-12 12:44:15 -0400

    Update StandardDeploymentGPO.ps1

--- StandardDeploymentGPO.ps1
+++ StandardDeploymentGPO.ps1
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <File clsid="{50BE44C8-567A-4ed1-B1D0-9234FE1F38AF}" name="LAPS" status="" image="1" changed="2019-06-09 15:36:07" uid="{931BB1C6-ED84-44EA-94E7-DFA2BCD8313F}" bypassErrors="1">
-   <Properties action="R" fromPath="\\share\Software\LAPS\*" targetPath="C:\BELOWAVERAGE\SOFTWARE\LAPS\" readOnly="0" archive="1" hidden="0" suppress="0" />
+   <Properties action="R" fromPath="\\share\Software\Deploy\LAPS\*" targetPath="C:\BELOWAVERAGE\SOFTWARE\Deploy\LAPS\" readOnly="0" archive="1" hidden="0" suppress="0" />
    <Filters>
       <FilterRunOnce hidden="1" not="0" bool="AND" id="{A13AC398-AEE1-40CF-B751-E12325969C6F}" />
    </Filters>
